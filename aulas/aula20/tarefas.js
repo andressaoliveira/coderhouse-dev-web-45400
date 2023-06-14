c09b7991db29fc0ca514c64942c9ebd54b77ec5c
Andressa Oliveira <andressa.pco@hotmail.com>
2023-06-13 22:00:38 -0300

    lista tarefas

--- aulas/aula20/tarefas.js
+++ aulas/aula20/tarefas.js
@@ -1,15 +1,31 @@
-
 let tarefas = [];
 
-function enviar(){
-    let input = document.getElementById("texto")
-    tarefas.push(input.value)
+
+//inserir dados no array
+let botaoEnviar = document.getElementById("enviar")
+botaoEnviar.addEventListener("click", acaoEnviar)
+
+let entrada = document.getElementById("entrada");
+
+function acaoEnviar() {
+    tarefas.push(entrada.value)
 }
 
-function visualizar(){
+
+//visualizar o array
+let botaoVisualizar = document.getElementById("visualizar")
+botaoVisualizar.addEventListener("click", acaoVisualizar)
+
+function acaoVisualizar() {
     console.log(tarefas)
 }
 
-function limpar(){
+
+//limpar o array
+let botaoLimpar = document.getElementById("limpar")
+botaoLimpar.addEventListener("click", acaoLimpar)
+
+function acaoLimpar() {
     tarefas = [];
+    console.log(tarefas);
 }
